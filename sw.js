bc47b0152ac9f8e7da6408027b2f8f1e6e3b30ea
KristenTonnelle <37288233+KristenTonnelle@users.noreply.github.com>
2021-05-08 10:49:23 +0200

    Update sw.js
    gerioù bet da gavout en eil pe eil partiadenn diwezhañ ne vint ket kinniget ken

--- sw.js
+++ sw.js
@@ -17,7 +17,7 @@ var filesToCache = [
 'son/klogorenn.ogg'
 ];
 
-var cacheName = 'km-1.6';
+var cacheName = 'km-1.7';
 self.addEventListener('install', function(e){
 	e.waitUntil(
 		caches.open(cacheName).then (function(cache) {
